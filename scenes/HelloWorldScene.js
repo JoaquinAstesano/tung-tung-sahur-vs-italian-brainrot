05a1f89eb24d48f51caa85f0ef90ba06dcd69c59
unknown <zilonkaleo@gmail.com>
2025-06-17 19:57:40 -0300

    agacharse-aereo

--- scenes/HelloWorldScene.js
+++ scenes/HelloWorldScene.js
@@ -1,104 +1,180 @@
 export default class HelloWorldScene extends Phaser.Scene {
-    constructor() {
-        super("hello-world");
-        this.ground = null;
-        this.tung = null;
-        this.obstacle = null;
-        this.collectible = null;
-        this.cursors = null;
-        this.gameSpeed = 200;
-        this.aereosGroup = null;
+  constructor() {
+    super("hello-world");
+    this.ground = null;
+    this.tung = null;
+    this.obstacle = null;
+    this.collectible = null;
+    this.cursors = null;
+    this.gameSpeed = 200;
+    this.aereosGroup = null;
+  }
+
+  init() {}
+  preload() {
+    //this.load.image("tung", "Public/assets/9eiAAB.png");
+    this.load.image("ground", "Public/assets/platforms.png");
+    // this.load.image('obstacle', 'Public/assets/slime_green.png');
+    this.load.image("collectible", "images/collectible.png");
+  }
+
+  create() {
+    //this.ground = this.add.image(400, 300, 800, 70, 'ground').setScale(4);
+    this.ground = this.add.tileSprite(400, 300, 800, 70, "ground");
+    this.physics.add.existing(this.ground);
+    this.physics.add.collider(this.ground, this.tung);
+    this.ground.body.immovable = true;
+    this.ground.body.allowGravity = false;
+
+    this.tung = this.physics.add.sprite(100, 150, "tung");
+    this.tung.setCollideWorldBounds(true);
+    this.tung.setGravityY(200);
+    this.tung.setScale(2, 2);
+    this.tung.setFlipX(true);
+
+    this.obstacle = this.physics.add.sprite(800, 250, "obstacle");
+    this.obstacle.setVelocityX(-this.gameSpeed);
+    this.obstacle.setImmovable(true);
+    this.obstacle.body.allowGravity = false;
+
+    this.aereoObstacle = this.physics.add.sprite(800, 150, "obstacle");
+    this.aereoObstacle.setVelocityX(-this.gameSpeed);
+    this.aereoObstacle.setImmovable(true);
+    this.aereoObstacle.body.allowGravity = false;
+    this.physics.add.collider(this.aereoObstacle, this.ground);
+    this.tungAereoObstacleCollider = this.physics.add.overlap(
+      this.tung,
+      this.aereoObstacle,
+      this.hitObstacle,
+      null,
+      this
+    );
+    this.aereoObstacle.x = 2000; // Start off-screen
+    this.nextIsAereo = false;
+
+    this.collectible = this.physics.add.sprite(400, 200, "collectible");
+    this.collectible.setScale(0.5);
+    this.collectible.body.allowGravity = false;
+
+    this.physics.add.collider(this.tung, this.ground);
+    this.physics.add.collider(this.obstacle, this.ground);
+    this.physics.add.collider(this.collectible, this.ground);
+    this.tungObstacleCollider = this.physics.add.overlap(
+      this.tung,
+      this.obstacle,
+      this.hitObstacle,
+      null,
+      this
+    );
+    this.physics.add.overlap(
+      this.tung,
+      this.collectible,
+      this.collectItem,
+      null,
+      this
+    );
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.input.keyboard.on("keydown-SPACE", this.jump, this);
+  }
+
+  collectItem(tung, collectible) {
+    collectible.disableBody(true, true);
+    console.log("Collectible picked up!");
+
+    this.tung.isImmune = true;
+
+    this.time.delayedCall(7000, () => {
+      this.tung.clearTint();
+      this.tung.isImmune = false;
+    });
+  }
+
+  jump() {
+    if (this.tung.body.touching.down) {
+      this.tung.setVelocityY(-600);
+    }
+  }
+
+  hitObstacle(tung, obstacle) {
+    obstacle.disableBody(true, false);
+
+    if (this.tung.isImmune) {
+      console.log("Tung is immune and avoids the obstacle!");
+      this.tung.isImmune = false;
+      return;
     }
 
-    init() {
+    //alert("¡Perdiste!");
+    console.log("Game Over! Restarting in 1 second...");
+    this.scene.restart();
+  }
+
+  update() {
+    this.ground.tilePositionX += this.gameSpeed * 0.02;
+
+    // Movimiento de tung con flechas izquierda y derecha
+    if (this.cursors.left.isDown) {
+      this.tung.setVelocityX(-200);
+      this.tung.setFlipX(true);
+    } else if (this.cursors.right.isDown) {
+      this.tung.setVelocityX(200);
+      this.tung.setFlipX(false);
+    } else {
+      this.tung.setVelocityX(0);
     }
-    preload() {
-        this.load.image('tung', 'images/tu-imagen-dino.png');
-        this.load.image('ground', 'Public/assets/platforms.png');
-       // this.load.image('obstacle', 'Public/assets/slime_green.png');
-        this.load.image('collectible', 'images/collectible.png');
-    }
 
-    create() {
-        //this.ground = this.add.image(400, 300, 800, 70, 'ground').setScale(4);
-        this.ground = this.add.tileSprite(400, 300, 800, 70, 'ground');
-        this.physics.add.existing(this.ground);
-        this.physics.add.collider(this.ground, this.tung);
-        this.ground.body.immovable = true;
-        this.ground.body.allowGravity = false;
+    //if (this.cursors.down.isDown) {
+    //this.tung.setScale(0.5, 1); // Más bajo
+    //this.tung.y = 150; // Baja un poco la posición
+    //} else {
+    //this.tung.setScale(0.1, 0.5); // Altura normal
+    //this.tung.y = 150; // Posición normal
+    //}
 
-        this.tung = this.physics.add.sprite(100, 250, 'tung');
-        this.tung.setCollideWorldBounds(true);
-        this.tung.setGravityY(200);
-        this.tung.setScale(1);
-
-        this.obstacle = this.physics.add.sprite(800, 250, 'obstacle');
+    if (!this.nextIsAereo) {
+      if (this.obstacle.x < -this.obstacle.width) {
+        this.obstacle.enableBody(
+          true,
+          this.obstacle.x,
+          this.obstacle.y,
+          true,
+          true
+        );
         this.obstacle.setVelocityX(-this.gameSpeed);
-        this.obstacle.setImmovable(true);
-        this.obstacle.body.allowGravity = false;
-
-        this.collectible = this.physics.add.sprite(400, 200, 'collectible');
-        this.collectible.setScale(0.5);
-        this.collectible.body.allowGravity = false;
-
-        this.physics.add.collider(this.tung, this.ground);
-        this.physics.add.collider(this.obstacle, this.ground);
-        this.physics.add.collider(this.collectible, this.ground);
-        this.tungObstacleCollider = this.physics.add.overlap(this.tung, this.obstacle, this.hitObstacle, null, this);
-        this.physics.add.overlap(this.tung, this.collectible, this.collectItem, null, this);
-
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.input.keyboard.on('keydown-SPACE', this.jump, this);
+        this.obstacle.x = 800 + Phaser.Math.Between(0, 300);
+        this.nextIsAereo = true;
+        this.aereoObstacle.x = 2000;
+      }
+    } else {
+      if (this.aereoObstacle.x < -this.aereoObstacle.width) {
+        this.aereoObstacle.enableBody(
+          true,
+          this.aereoObstacle.x,
+          this.aereoObstacle.y,
+          true,
+          true
+        );
+        this.aereoObstacle.setVelocityX(-this.gameSpeed);
+        this.aereoObstacle.x = 800 + Phaser.Math.Between(0, 300);
+        this.nextIsAereo = false;
+        this.obstacle.X = 2000;
+      }
     }
 
-    collectItem(tung, collectible) {
-        collectible.disableBody(true, true);
-        console.log("Collectible picked up!");
-
-        this.tung.isImmune = true;
-
-    
-
-        this.time.delayedCall(7000, () => {
-            this.tung.clearTint();
-            this.tung.isImmune = false;
-        });
+    if (this.collectible.x < -this.collectible.width) {
+      this.collectible.x = 800 + Phaser.Math.Between(0, 300);
     }
 
-    jump() {
-        if (this.tung.body.touching.down) {
-            this.tung.setVelocityY(-600);
-        }
+    this.collectible.x -= this.gameSpeed * 0.02;
+    this.obstacle.x -= this.gameSpeed * 0.02;
+    this.aereoObstacle.x -= this.gameSpeed * 0.02;
+
+    if (this.tung.x < 0) {
+      this.tung.x = 0;
     }
-
-    hitObstacle(tung, obstacle) {
-        obstacle.disableBody(true, false)
-
-        if (this.tung.isImmune) {
-            console.log("Tung is immune and avoids the obstacle!");
-            this.tung.isImmune = false;
-            return;
-        }
-        
-        //alert("¡Perdiste!");
-        console.log("Game Over! Restarting in 1 second...");
-        this.scene.restart();
+    if (this.tung.x > 300) {
+      this.tung.x = 300;
     }
-
-    update() {
-        this.ground.tilePositionX += this.gameSpeed * 0.02;
-
-        if (this.obstacle.x < -this.obstacle.width) {
-            this.obstacle.enableBody(true, this.obstacle.x,  this.obstacle.y, true, true);
-            this.obstacle.setVelocityX(-this.gameSpeed);
-            this.obstacle.x = 800 + Phaser.Math.Between(0, 300);
-        }
-
-        if (this.collectible.x < -this.collectible.width) {
-            this.collectible.x = 800 + Phaser.Math.Between(0, 300);
-        }
-
-        this.collectible.x -= this.gameSpeed * 0.02;
-        this.obstacle.x -= this.gameSpeed * 0.02;
-    }
+  }
 }
